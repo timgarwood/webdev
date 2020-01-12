3809b21deeac80d842e4cb1dfc76621cddec7212
unknown <timgarwood@gmail.com>
2020-01-12 15:26:55 -0500

    canvas demo

---
+++ canvas/map.js
@@ -0,0 +1,27 @@
+function getMapConfiguration() {
+    return {
+        imgUrl: "./floor.jpg",
+        locations: [
+            {
+                name: "Warehouse A",
+                x: 20,
+                y:190
+            },
+            {
+                name: "Warehouse B",
+                x: 81,
+                y: 94
+            },
+            {
+                name: "Warehouse C",
+                x: 270,
+                y: 315 
+            },
+            {
+                name: "Warehouse D",
+                x: 548,
+                y: 262
+            }
+        ]
+    };
+}
